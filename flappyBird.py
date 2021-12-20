9d921738a382e41efdd7fc8ca512ddf32268ebc2
Ronaldo Guilherme <rmdelgado@uta.cv>
2021-12-20 01:50:20 -0100

    Metodo dos canos feito

--- flappyBird.py
+++ flappyBird.py
@@ -1,3 +1,4 @@
+from _typeshed import Self
 import pygame #bilbioteca para jogos
 import os #integrar codigos com os arquivos do pc
 import random #Gerar Numeros aleatorios 
@@ -89,14 +90,48 @@ class passaro:
     def get_mask(self):
         pygame.mask.from_surface(self.imagen)
         
-        
-        
     
-    
-    
-
 class chao:
-    pass
+    DISTANCIA = 200
+    VELOCIDADE = 5
+    def __init__(self, x):
+        self.x = x
+        self.altura = 0
+        self.pos_top = 0
+        self.pos_base = 0
+        self.CANO_TOPO = pygame.transform.flip(IMAGEN_CANO, False, True)
+        self.CANO_BASE = IMAGEN_CANO
+        self.passou = False
+        self.definir_altura()
+    
+    def definir_altura(self):
+        self.altura = random.randrange(50 , 450)
+        self.pos_base = self.altura - self.CANO_TOPO.get_heigth()
+        self.pos_base = self.altura - DISTANCIA
+
+    def mover(self):
+        self.x -= self.VELOCIDADE
+
+    def desenhar(self, tela):
+        tela.blit(self.CANO_TOPO,(self.x, self.pos_top))
+        tela.blit(self.CANO_BASE,(self.x, self.pos_base))
+    
+    def colidri(self,passaro):
+        passaro_mak = passaro.get_mask()
+        topo_mask = pygame.mask.from_surface(self.CANO_TOPO)
+        base_mask = pygame.mask.from_surface(self.CANO_BASE)
+
+        distancia_topo = (self.x - passaro.x, self.pos_top - round(passaro.y))
+        distancia_base = (self.x - passaro.x, self.pos_base - round(passaro.y))
+
+        topo_ponto = passaro_mak.overlap(topo_mask,distancia_topo)
+        base_ponto = passaro_mak.overlap(base_mask,distancia_base)
+
+        if base_ponto or topo_ponto:
+            return True
+        else:
+            return False
+
 
 class cano:
     pass
